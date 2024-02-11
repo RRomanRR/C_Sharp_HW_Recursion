bb65a3c3dea27e754f885b806334d8160ab65a9f
Roman <pliskarv@yandex.ru>
2024-02-11 16:33:30 +0900

    homework_done

--- Lesson_7/task_1/Program.cs
+++ Lesson_7/task_1/Program.cs
@@ -1,22 +1,22 @@
 ﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все 
 // натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
-Не успел доделать :-)
 
+Console.Clear();
 Console.Write("Введите значение M: ");
-int m = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine()!);
 Console.Write("Введите значение N: ");
 int n = Convert.ToInt32(Console.ReadLine()!);
-
 void EvenNumber(int m, int n)
 {
-    int a = n-m;
+    int a = n-m +1;
     if (m > n)
         return;
     int i = 0;
     if (i < a)
     {
-        Console.Write($"{i}, ");
+        Console.Write($"{m}, ");
         i = +1;
     }
+    EvenNumber(m+1,n);
 }
 EvenNumber(m,n);
