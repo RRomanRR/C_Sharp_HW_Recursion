0a1dc001c88847669027f2fdcb34a39899105717
Roman <pliskarv@yandex.ru>
2024-02-11 14:59:42 +0900

    no solved task1

--- Lesson_7/task_1/Program.cs
+++ Lesson_7/task_1/Program.cs
@@ -1,5 +1,22 @@
 ﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все 
 // натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+Не успел доделать :-)
 
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine()!);
 
-
+void EvenNumber(int m, int n)
+{
+    int a = n-m;
+    if (m > n)
+        return;
+    int i = 0;
+    if (i < a)
+    {
+        Console.Write($"{i}, ");
+        i = +1;
+    }
+}
+EvenNumber(m,n);
--- Lesson_7/task_2.1/Program.cs
+++ Lesson_7/task_2.1/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Задача 2.1:Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
+// Не смог решить
 
-
-int Ankerman (int n, int m)
-{
-        if (n < 0 || m < 0) throw new ArgumentOutOfRangeException();
-        if (m == 0) 
-          return (n - 1, 1);
-        if (m == 0) 
-          return ( n - 1, m);
-          return ( n - 1, (n, m - 1));
-}
-int n = 5;
-int m = 1;
-Ankerman(n, m);
+// int Ankerman (int n, int m)
+// {
+//         if (n < 0 || m < 0) throw new ArgumentOutOfRangeException();
+//         if (m == 0) 
+//           return (n - 1, 1);
+//         if (m == 0) 
+//           return ( n - 1, m);
+//           return ( n - 1, (n, m - 1));
+// }
+// int n = 5;
+// int m = 1;
+// Ankerman(n, m);
