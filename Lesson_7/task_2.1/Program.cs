189a4e7a73f602274c61fc90303c541fb0e80698
Roman <pliskarv@yandex.ru>
2024-02-11 14:40:31 +0900

    solved task2.23

--- Lesson_7/task_2.1/Program.cs
+++ Lesson_7/task_2.1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2.1:Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+
+int Ankerman (int n, int m)
+{
+        if (n < 0 || m < 0) throw new ArgumentOutOfRangeException();
+        if (m == 0) 
+          return (n - 1, 1);
+        if (m == 0) 
+          return ( n - 1, m);
+          return ( n - 1, (n, m - 1));
+}
+int n = 5;
+int m = 1;
+Ankerman(n, m);
